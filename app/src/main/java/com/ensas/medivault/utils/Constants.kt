792b34429d52ea95d819fc732755b3adc48f2bf1
PhoenixHO <oussamahaddouche0@gmail.com>
2025-01-03 22:07:53 +0100

    refactor(utils): remove unused Constants and Extensions classes

--- app/src/main/java/com/ensas/medivault/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.ensas.medivault.utils
-
-// For the constants used in the app (e.g. keys, URLs)
-class Constants {
-}
--- app/src/main/java/com/ensas/medivault/utils/Extensions.kt
+++
@@ -1,6 +0,0 @@
-package com.ensas.medivault.utils
-
-// For the extensions used in the app
-// Extensions are used to add new functionality to existing classes without modifying them
-class Extensions {
-}
